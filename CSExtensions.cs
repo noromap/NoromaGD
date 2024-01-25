feb66614f303f31c5c266beb684c2bb4b6037623
noromap <noromap.ai@gmail.com>
2024-01-26 00:37:30 +0900

    initial commit

---
+++ CSExtensions.cs
@@ -0,0 +1,103 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NoromaGD
+{
+    public static class CSExtensions
+    {
+        #region IEnumerable
+
+        public static void Do<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (var item in source)
+            {
+                action(item);
+            }
+        }
+
+        #endregion IEnumerable
+
+        #region Int
+
+        public static float SignFloat(this int value)
+        {
+            return Mathf.Sign(value);
+        }
+
+        public static int Sign(this int value)
+        {
+            return Mathf.Sign(value);
+        }
+
+        #endregion Int
+
+        #region Float
+
+        public static float Abs(this float x)
+        {
+            return Mathf.Abs(x);
+        }
+
+        public static float Sign(this float x)
+        {
+            return Mathf.Sign(x);
+        }
+
+        public static float DegToRad(this float x)
+        {
+            return Mathf.DegToRad(x);
+        }
+
+        public static float RadToDeg(this float x)
+        {
+            return Mathf.RadToDeg(x);
+        }
+
+        public static float Pow(this float x, float y)
+        {
+            return Mathf.Pow(x, y);
+        }
+
+        #endregion Float
+
+        #region double
+
+        public static double Abs(this double x)
+        {
+            return Mathf.Abs(x);
+        }
+
+        public static double Sign(this double x)
+        {
+            return Mathf.Sign(x);
+        }
+
+        public static double DegToRad(this double x)
+        {
+            return Mathf.DegToRad(x);
+        }
+
+        public static double RadToDeg(this double x)
+        {
+            return Mathf.RadToDeg(x);
+        }
+
+        public static double Pow(this double x, double y)
+        {
+            return Mathf.Pow(x, y);
+        }
+
+        #endregion double
+
+        #region string
+
+        public static bool IsNullOrEmpty(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+
+        #endregion string
+    }
+}
---
+++ GDExtensions.cs
@@ -0,0 +1,141 @@
+using Godot;
+using System.Collections.Generic;
+
+namespace NoromaGD
+{
+    public static class GDExtensions
+    {
+        #region CollisionShape2D
+
+        public static Rect2 ToRect(this CollisionShape2D collisionShape)
+        {
+            var rect = collisionShape.Shape.GetRect();
+            rect = new Rect2(rect.Position + collisionShape.GlobalPosition, rect.Size);
+            return rect;
+        }
+
+        #endregion CollisionShape2D
+
+        #region Rect2
+
+        /// <summary>
+        /// 左上　※Y軸上方向はマイナス　Y軸下方向はプラス
+        /// </summary>
+        /// <param name="rect"></param>
+        /// <returns></returns>
+        public static Vector2 TopLeft(this Rect2 rect)
+        {
+            return rect.Position;
+        }
+
+        /// <summary>
+        /// 右上　※Y軸上方向はマイナス　Y軸下方向はプラス
+        /// </summary>
+        /// <param name="rect"></param>
+        /// <returns></returns>
+        public static Vector2 TopRight(this Rect2 rect)
+        {
+            return new Vector2(rect.End.X, rect.Position.Y);
+        }
+
+        public static Vector2 BottomLeft(this Rect2 rect)
+        {
+            return new Vector2(rect.Position.X, rect.End.Y);
+        }
+
+        public static Vector2 BottomRight(this Rect2 rect)
+        {
+            return new Vector2(rect.End.X, rect.End.Y);
+        }
+
+        public static Vector2 Top(this Rect2 rect)
+        {
+            return new Vector2(rect.GetCenter().X, rect.Position.Y);
+        }
+
+        public static Vector2 Right(this Rect2 rect)
+        {
+            return new Vector2(rect.End.X, rect.GetCenter().Y);
+        }
+
+        public static Vector2 Left(this Rect2 rect)
+        {
+            return new Vector2(rect.Position.X, rect.GetCenter().Y);
+        }
+
+        public static Vector2 Bottom(this Rect2 rect)
+        {
+            return new Vector2(rect.GetCenter().X, rect.End.Y);
+        }
+
+        #endregion Rect2
+
+        #region Node
+
+        public static T GetChildByType<T>(this Node node, bool recursive = true)
+            where T : class
+        {
+            int childCount = node.GetChildCount();
+
+            for (int i = 0; i < childCount; i++)
+            {
+                Node child = node.GetChild(i);
+                if (child is T childT)
+                    return childT;
+
+                if (recursive && child.GetChildCount() > 0)
+                {
+                    T recursiveResult = child.GetChildByType<T>(true);
+                    if (recursiveResult != null)
+                        return recursiveResult;
+                }
+            }
+
+            return null;
+        }
+
+        public static T GetParentByType<T>(this Node node) where T : class
+        {
+            Node parent = node.GetParent();
+            if (parent != null)
+            {
+                if (parent is T parentT)
+                {
+                    return parentT;
+                }
+                else
+                {
+                    return parent.GetParentByType<T>();
+                }
+            }
+
+            return null;
+        }
+
+        public static List<T> GetChildrenByType<T>(this Node node) where T : class
+        {
+            List<T> children = new List<T>();
+            node.GetChildrenByType(children);
+            return children;
+        }
+
+        private static void GetChildrenByType<T>(this Node node, List<T> children) where T : class
+        {
+            if (children == null) children = new List<T>();
+            int childCount = node.GetChildCount();
+            for (int i = 0; i < childCount; i++)
+            {
+                Node child = node.GetChild(i);
+                if (child is T childT)
+                    children.Add(childT);
+
+                if (child.GetChildCount() > 0)
+                {
+                    child.GetChildrenByType(children);
+                }
+            }
+        }
+
+        #endregion Node
+    }
+}
