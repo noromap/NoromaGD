6f082160cc70ddbbdf80190e07503688b10106b7
noromap <noromap.ai@gmail.com>
2024-02-03 21:18:25 +0900

    Create AdditiveTimer.cs

---
+++ Utilities/AdditiveTimer.cs
@@ -0,0 +1,30 @@
+namespace NoromaGD
+{
+    public class AdditiveTimer : TimerBase
+    {
+        public AdditiveTimer(float duration) : base(duration)
+        {
+        }
+
+        public override bool IsInTime()
+        {
+            return currentTime < duration;
+        }
+
+        public override void Reset()
+        {
+            currentTime = duration;
+        }
+
+        public override void Set()
+        {
+            currentTime = 0;
+        }
+
+        protected override void OnUpdate(float delta)
+        {
+            currentTime += delta;
+            if (currentTime > duration) currentTime = duration;
+        }
+    }
+}
