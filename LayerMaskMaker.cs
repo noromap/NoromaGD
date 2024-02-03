d698c1417002d9179af90b14e090547738c337b7
noromap <noromap.ai@gmail.com>
2024-02-03 14:28:23 +0900

    Update LayerMaskMaker.cs

--- LayerMaskMaker.cs
+++ LayerMaskMaker.cs
@@ -7,7 +7,8 @@ namespace NoromaGD
 {
     public static class LayerMaskMaker
     {
-        public static Dictionary<string, int> Layers { get; private set; } = new Dictionary<string, int>();
+        public static Dictionary<string, UInt32> Layers { get; private set; } = new Dictionary<string, UInt32>();
+        public static Dictionary<string, UInt32> LayerValues { get; private set; } = new Dictionary<string, UInt32>();
 
         static LayerMaskMaker()
         {
@@ -15,8 +16,21 @@ namespace NoromaGD
             {
                 var layer = ProjectSettings.GetSetting($"layer_names/2d_physics/layer_{i + 1}");
                 if (layer.Obj != null && string.IsNullOrEmpty(layer.AsString()) == false)
-                    Layers[layer.AsString()] = (int)Math.Pow(2, i);
+                {
+                    Layers[layer.AsString()] = (uint)Math.Pow(2, i);
+                    LayerValues[layer.AsString()] = (uint)i + 1;
+                }
             }
+        }
+
+        public static int GetLayer(string name)
+        {
+            if (LayerValues.ContainsKey(name) == false)
+            {
+                GD.PrintErr($"Layer : {name} は存在しません。");
+                return -1;
+            }
+            return (int)LayerValues[name];
         }
 
         public static UInt32 MakeMask(params string[] layers)
@@ -26,7 +40,25 @@ namespace NoromaGD
             {
                 GD.Print($"一致するレイヤーが存在しません。レイヤー：{string.Join(',', layers)}");
             }
-            return (UInt32)im.Select(l => l.Value).Sum();
+            return (UInt32)im.Select(l => l.Value).Sum(v => v);
+        }
+
+        public static UInt32 AddLayerToMask(UInt32 mask, params string[] layers)
+        {
+            foreach (var layer in layers)
+            {
+                mask = mask | Layers[layer];
+            }
+            return mask;
+        }
+
+        public static UInt32 RemoveLayerFromMask(UInt32 mask, params string[] layers)
+        {
+            foreach (var layer in layers)
+            {
+                mask = mask & ~Layers[layer];
+            }
+            return mask;
         }
     }
 }
